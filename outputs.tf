b1c91d405d6193a8f1b7081ec31ed3567eba69e0
Ricardo Cordeiro <ricardo.cordeiro@tux.com.pt>
2020-10-29 17:53:55 +0000

    Adds license headers

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,17 @@
+# Copyright 2020 Ricardo Cordeiro <ricardo.cordeiro@tux.com.pt>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 output "nat_ips" {
   description = "List of external IP addresses allocated to the Cloud NAT instance"
   value       = google_compute_router_nat.nat_router_config.nat_ips
